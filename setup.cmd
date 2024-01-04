f93b231acf0d5af39a04fd1438eb0329dbb4b0c5
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-03 19:38:33 -0800

    Update 'setup.cmd' to install pipx and poetry
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- setup.cmd
+++ setup.cmd
@@ -1,9 +1,20 @@
 @echo off
 setlocal EnableExtensions
+    call sudo py -3 -m pip install --upgrade pip
+    call py -3 -m pip install --user -r "%~dp0requirements.txt"
+
+    call scoop install pipx
+    call scoop update pipx
+    call pipx install poetry
+
+    set VIRTUAL_ENV_DISABLE_PROMPT=1
+    call poetry shell
+
     call poetry run pip install --upgrade setuptools wheel
     call poetry run pip install --upgrade pytest-github-actions-annotate-failures
     call poetry install --no-interaction --with dev --with speedups
+    call poetry run black .
     call poetry run poe yarn
     call poetry run poe lint
     call poetry run poe yarn test
-endlocal & goto:eof
+endlocal & exit /b 0
